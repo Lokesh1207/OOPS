4065da5021521f801f7013fcf81595648581a283
Lokesh S <157147364+Lokesh1207@users.noreply.github.com>
2024-03-26 22:26:30 +0530

    Add files via upload
    new commit

---
+++ Package1/Example.java
@@ -0,0 +1,11 @@
+package Package1;
+
+public class Example {
+    public static void printMessage() {
+        System.out.println("Hello!,I am Learning Java Everyday.");
+    }
+
+    public static void main(String[] args) {
+        printMessage(); // calling the method from within the class
+    }
+}
